e9e4927672aa78e311f6305056d410c354d5f384
Sergey <pythontestflask@gmail.com>
2022-03-02 20:09:22 +0200

    make it better

--- app/main_page/main_page.py
+++ app/main_page/main_page.py
@@ -1,16 +1,16 @@
 from flask import Blueprint, render_template, session
 from app.models import Goods, Img
+from app import app
 
-main_page_bp = Blueprint('main_page',__name__)
 
 
-@main_page_bp.route('/')
+@app.route('/')
 def main_page():
    goods = Goods.query.order_by(Goods.price).all() 
    image = Img.query.order_by(Img.id).all()                     
    return render_template('main_page/main_page.html', data=[goods,image,session])
 
 
-@main_page_bp.route('/about')
+@app.route('/about')
 def about():   
    return render_template('main_page/about.html',data=session)
